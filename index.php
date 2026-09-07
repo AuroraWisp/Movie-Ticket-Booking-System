689fed7594dbd2dd989af668ab00bd08efa480ee
Saiyara Adiba <saiyaraadiba123@gmail.com>
2026-09-08 02:46:46 +0600

    Fix structure of index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cineverse</title>
+    <title>CineVerse</title>
     <link rel="stylesheet" href="style.css">
     <style>
         .welcome-card {
